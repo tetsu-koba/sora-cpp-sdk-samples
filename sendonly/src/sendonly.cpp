914e0067fbb02fd34517bf9bb1c7e296cd425849
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2022-07-01 06:32:14 +0000

    Add --video-bit-rate and --audio-bit-rate

--- sendonly/src/sendonly.cpp
+++ sendonly/src/sendonly.cpp
@@ -28,6 +28,8 @@ struct SendOnlyConfig : sora::SoraDefaultClientConfig {
   std::string metadata;
   bool no_video_device = false;
   bool no_audio_device = false;
+  int video_bit_rate = 0;
+  int audio_bit_rate = 0;
   std::string video_device = "";
   std::string resolution = "VGA";
   int framerate = 30;
@@ -127,6 +129,8 @@ class SendOnly : public std::enable_shared_from_this<SendOnly>,
     config.channel_id = config_.channel_id;
     config.multistream = config_.multistream;
     config.video_codec_type = config_.video_codec_type;
+    config.video_bit_rate = config_.video_bit_rate;
+    config.audio_bit_rate = config_.audio_bit_rate;
 
     // メタデータのパース
     if (!config_.metadata.empty()) {
@@ -252,6 +256,12 @@ int main(int argc, char* argv[]) {
   app.add_option("--video-codec-type", config.video_codec_type,
                  "Video codec for send")
       ->check(CLI::IsMember({"", "VP8", "VP9", "AV1", "H264"}));
+  app.add_option("--video-bit-rate", config.video_bit_rate,
+                   "Video bit rate")
+      ->check(CLI::Range(0, 30000));
+  app.add_option("--audio-bit-rate", config.audio_bit_rate,
+                   "Audio bit rate")
+      ->check(CLI::Range(0, 510));
   app.add_option("--multistream", config.multistream,
                  "Use multistream (default: false)")
       ->transform(CLI::CheckedTransformer(bool_map, CLI::ignore_case));
